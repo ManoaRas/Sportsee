68669d21a7995862d144efbfd538e2309c23668a
Manoa Rasolomalala <manoatony.pro@gmail.com>
2024-08-19 20:27:14 +0200

    Add mocks and services for usersInfos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,56 @@
+import { useEffect, useState } from 'react'
+import {
+  getUserInfos,
+  // getActivity,
+  // getAverageSessions,
+  // getPerformance
+} from '../services/api'
+
 export function Home() {
+  const [userInfos, setUserInfos] = useState(null)
+  // const [activityInfos, setActivityInfos] = useState(null)
+  // const [averageSessionsInfos, setAverageSessionsInfos] = useState(null)
+  // const [performanceInfos, setPerformanceInfos] = useState(null)
+
+  const userId = 12
+
+  const fetchData = async () => {
+    try {
+      const userData = await getUserInfos(userId)
+      // const activityData = await getActivity(activityInfos)
+      // const averageSessionsData = await getAverageSessions(averageSessionsInfos)
+      // const performanceData = await getPerformance(performanceInfos)
+
+      setUserInfos(userData)
+      // setActivityInfos(activityData)
+      // setAverageSessionsInfos(averageSessionsData)
+      // setPerformanceInfos(performanceData)
+    } catch (err) {
+      alert('Une erreur est survenue lors du chargement de données !')
+    }
+  }
+
+  useEffect(() => { fetchData() }, [])
+
   return (
     <section>
-      <div>
-        <h1 className=''>
-          Bonjour
-          {/* <span className=''>{userInfos.data.userInfos.firstName}</span> */}
-        </h1>
+      {userInfos ? (
+        <>
+          <h1 className=''>
+            Bonjour
+            <span className=''>{userInfos.data.userInfos.firstName}</span>
+          </h1>
 
-        <p className=''>Félicitations ! Vous avez explosé vos objectifs hier 👏</p>
+          <p className=''>Félicitations ! Vous avez explosé vos objectifs hier 👏</p>
 
-        <div className=''></div>
+          <div className=''>
+          </div>
 
-        <div className=''></div>
-      </div>
+          <div className=''></div>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </section>
   )
 }
---
+++ src/services/api.js
@@ -0,0 +1,48 @@
+import axios from 'axios'
+// import activity from '../mocks/activity.json'
+// import averageSessions from '../mocks/averageSessions.json'
+import mainData from '../mocks/mainData.json'
+// import performance from '../mocks/performance.json'
+
+const BASE_URL = 'http://localhost:3000/user'
+const isMock = false
+
+export async function getUserInfos(id) {
+  if (isMock === true) {
+    return mainData
+  } else {
+    return await axios
+      .get(`${BASE_URL}/${id}`)
+      .then((resp) => resp.data)
+  }
+}
+
+// export async function getActivity(id) {
+//   if (isMock === true) {
+//     return activity
+//   } else {
+//     return await axios
+//       .get(`${BASE_URL}/${id}/activity`)
+//       .then((resp) => resp.data)
+//   }
+// }
+
+// export async function getAverageSessions(id) {
+//   if (isMock === true) {
+//     return averageSessions
+//   } else {
+//     return await axios
+//       .get(`${BASE_URL}/${id}/average-sessions`)
+//       .then((resp) => resp.data)
+//   }
+// }
+
+// export async function getPerformance(id) {
+//   if (isMock === true) {
+//     return performance
+//   } else {
+//     return await axios
+//       .get(`${BASE_URL}/${id}/performance`)
+//       .then((resp) => resp.data)
+//   }
+// }
